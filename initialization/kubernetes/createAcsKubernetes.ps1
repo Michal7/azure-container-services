71e6b50f8f163505311da9ec9e176606bb45503d
damian.mazurek <damian.mazurek@elmodis.com>
2017-10-14 13:54:32 +0200

    Add UI kubernetes command

--- initialization/kubernetes/createAcsKubernetes.ps1
+++ initialization/kubernetes/createAcsKubernetes.ps1
@@ -5,3 +5,6 @@ az group create --name $resourceGroupName --location westeurope
 az acs create --orchestrator-type=kubernetes --resource-group $resourceGroupName --name=$kubernetesName --generate-ssh-keys 
 az acs kubernetes install-cli 
 az acs kubernetes get-credentials --resource-group=$resourceGroupName --name=$kubernetesName
+
+#Install web UI if not present
+#kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
